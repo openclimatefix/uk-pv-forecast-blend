008b2bd898c5bce8a519089ce94e51821a146b1d
BumpVersion Action <bumpversion@github-actions>
2025-03-13 11:24:32 +0000

    Bump version: 1.0.5 → 1.0.6 [skip ci]

--- forecast_blend/app.py
+++ forecast_blend/app.py
@@ -35,7 +35,7 @@ from weights import weights, model_names
 
 logger = structlog.stdlib.get_logger()
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
